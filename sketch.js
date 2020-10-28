d99ab2e484c65d99bb8bf495c42ed8a4e8bb9397
vabhikgarg <73597098+vabhikgarg@users.noreply.github.com>
2020-10-28 21:44:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var sword,Image1,score;
+var gameState=1;
+var PLAY=1;
+var END=0;
+
+
+
+
+function preload(){
+  image1=loadImage(sword.png)
+ 
+}
+
+function Draw(){
+drawSprites()
+
+
+
+
+
+
+}
+function sword(){
+ sword.addImage(image1) 
+  
+  
+  
+  
+  
+}
